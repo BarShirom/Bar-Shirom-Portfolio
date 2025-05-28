bd27b3f4e4e784450aaf1497d21d6c634b0270b5
BarShirom <shirombar@gmail.com>
2025-05-28 13:26:38 +0300

    add skiils

---
+++ client/src/data/skillsData.ts
@@ -0,0 +1,57 @@
+export const skills = [
+  {
+    name: "HTML5",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    link: "https://www.w3.org/html/",
+  },
+  {
+    name: "CSS3",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+    link: "https://www.w3schools.com/css/",
+  },
+  {
+    name: "JavaScript",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    name: "TypeScript",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    link: "https://www.typescriptlang.org/",
+  },
+  {
+    name: "React",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+    link: "https://reactjs.org/",
+  },
+  {
+    name: "React Native",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    link: "https://reactnative.dev/",
+  },
+  {
+    name: "Redux",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+    link: "https://redux.js.org",
+  },
+  {
+    name: "Node.js",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+    link: "https://nodejs.org",
+  },
+  {
+    name: "Express",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+    link: "https://expressjs.com",
+  },
+  {
+    name: "MongoDB",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+    link: "https://www.mongodb.com/",
+  },
+  {
+    name: "MySQL",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+    link: "https://www.mysql.com/",
+  },
+];
